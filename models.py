92bcb21273467139522aa15d3367dd3bc996e3c6
sswoong2000 <swgy0305@gmail.com>
2025-11-18 06:26:43 +0900

    Create models.py

---
+++ models.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel, EmailStr
+from typing import Optional
+
+
+class UserCreate(BaseModel):
+    email: EmailStr
+    password: str
+
+
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str
+
+
+class User(BaseModel):
+    id: int
+    email: EmailStr
+    class Config:
+        orm_mode = True
