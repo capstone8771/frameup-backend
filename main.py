0e66160b8fdcf176d762671691bd5641fcecb0ea
sswoong2000 <swgy0305@gmail.com>
2025-11-18 00:14:12 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
-from fastapi import FastAPI
-from db import get_connection
+from fastapi import FastAPI, Request
 
 app = FastAPI()
 
-@app.get("/test-db")
-def test_db():
-    try:
-        conn = get_connection()
-        cursor = conn.cursor()
-        cursor.execute("SELECT NOW()")
-        result = cursor.fetchone()
-        conn.close()
-        return {"success": True, "db_time": result}
-    except Exception as e:
-        return {"success": False, "error": str(e)}
+@app.post("/submit")
+async def submit_data(request: Request):
+    data = await request.json()
+    print("from frontend:", data)
+    return {"message": "ok"}
